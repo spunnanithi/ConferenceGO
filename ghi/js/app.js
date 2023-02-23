5a9e32ef7b7e7ff67ecab1e1b0899d940d07faf5
Sirasit Punnanithi <spunnanithi@gmail.com>
2023-02-22 16:22:31 -0800

    Implemented card templates and grid layout

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,3 +1,15 @@
+function createCard(name, description, pictureUrl) {
+    return `
+    <div class="card shadow p-3 mb-5 bg-body-tertiary rounded">
+        <img src="${pictureUrl}" class="card-img-top">
+        <div class="card-body">
+        <h5 class="card-title">${name}</h5>
+        <p class="card-text">${description}</p>
+        </div>
+    </div>
+    `;
+}
+
 // Since we are using 'await' keyword, we need to write 'async' keyword next to function declaration
 window.addEventListener('DOMContentLoaded', async () => {
 
@@ -17,21 +29,29 @@ window.addEventListener('DOMContentLoaded', async () => {
             // response.json() returns a Promise object so we need 'await' keyword
             const data = await response.json();
 
-            const conference = data.conferences[0];
-            const nameTag = document.querySelector('.card-title');
-            nameTag.innerHTML = conference.name;
+            let count = 0
+            for (let conference of data.conferences) {
 
-            const detailUrl = `http://localhost:8000${conference.href}`;
-            const detailResponse = await fetch(detailUrl);
-            if (detailResponse.ok) {
-                const details = await detailResponse.json();
-                console.log(details);
+                const detailUrl = `http://localhost:8000${conference.href}`;
+                const detailResponse = await fetch(detailUrl);
 
-                const descriptionTag = document.querySelector('.card-text');
-                descriptionTag.innerHTML = details.conference.description;
+                if (detailResponse.ok) {
+                    const details = await detailResponse.json();
+                    const title = details.conference.name;
+                    const description = details.conference.description;
+                    const pictureUrl = details.conference.location.picture_url;
+                    const html = createCard(title, description, pictureUrl);
 
-                const imageTag = document.querySelector('.card-img-top');
-                imageTag.src = details.conference.location.picture_url;
+                    const columns = document.querySelectorAll('.col'); // ['.col', '.col', '.col']
+
+                    const column = columns[count];
+                    column.innerHTML += html;
+                    count++;
+                    if (count === 3) {
+                        count = 0;
+                    }
+
+                }
             }
 
         }
